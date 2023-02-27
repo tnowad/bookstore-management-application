4a0095f5b8bf099e66e46e5c1106187e55fc4659
Nguyen Minh Tuan <tnowad@gmail.com>
2023-02-27 20:16:34 +0700

    something went wrong!

---
+++ src/main/java/com/bookstore/BookstoreManagementApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bookstore;
+
+/**
+ *
+ * @author alex
+ */
+public class BookstoreManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
