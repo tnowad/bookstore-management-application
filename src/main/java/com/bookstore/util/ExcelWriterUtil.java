b6e9540fe49bc223034f4bc5c7af9acefbc31ce4
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2023-03-13 07:11:45 +0700

    Fixed ExcelWriterUtil

--- src/main/java/com/bookstore/util/ExcelWriterUtil.java
+++ src/main/java/com/bookstore/util/ExcelWriterUtil.java
@@ -1,13 +1,15 @@
 package com.bookstore.util;
 
-import java.io.File;
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -19,20 +21,29 @@ public class ExcelWriterUtil {
     Objects.requireNonNull(data, "Data cannot be null");
     Objects.requireNonNull(filePath, "File path cannot be null");
     Objects.requireNonNull(sheetName, "Sheet name cannot be null");
-    Files.createDirectories(Paths.get(filePath).getParent());
+
+    Path parentDirectory = Paths.get(filePath).getParent();
+    if (parentDirectory != null && !Files.exists(parentDirectory)) {
+      Files.createDirectories(parentDirectory);
+    }
+
     SXSSFWorkbook workbook = new SXSSFWorkbook();
     Sheet sheet = workbook.createSheet(sheetName);
-    for (List<String> rowData : data) {
-      Row row = sheet.createRow(sheet.getLastRowNum() + 1);
-      int columnIndex = 0;
-      for (String cellData : rowData) {
-        Cell cell = row.createCell(columnIndex++);
-        Optional.ofNullable(cellData).ifPresent(cell::setCellValue);
+
+    try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
+      for (List<String> rowData : data) {
+        Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+
+        int columnIndex = 0;
+        for (String cellData : rowData) {
+          Cell cell = row.createCell(columnIndex++);
+          Optional.ofNullable(cellData).ifPresent(cell::setCellValue);
+        }
       }
+
+      workbook.write(bos);
+    } finally {
+      workbook.close();
     }
-    try (FileOutputStream outputStream = new FileOutputStream(new File(filePath))) {
-      workbook.write(outputStream);
-    }
-    workbook.close();
   }
 }
