ecb33f183e5cad1af4918821aecbc5e6cc3562b9
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-14 00:52:46 +0700

    create: initDatabase

--- database/init/insert.sql
+++ database/init/insert.sql
@@ -1,1 +1,93 @@
 -- Here is the SQL script to insert the data into the database tables.
+
+INSERT INTO
+    users (
+        username,
+        password,
+        status,
+        name,
+        email,
+        phone,
+        role
+    )
+VALUES (
+        'tranvanta',
+        'tranvanta123',
+        'active',
+        'Trần Văn Tài',
+        'tranvanta1234@gmail.com',
+        '0862999887',
+        'customer'
+    ), (
+        'lethianh',
+        'lethianh123',
+        'active',
+        'Lê Thị Anh',
+        'lethianh1234@gmail.com',
+        '0905999777',
+        'customer'
+    ), (
+        'phuongthanh',
+        'phuongthanh123',
+        'active',
+        'Phương Thanh',
+        'phuongthanh1234@gmail.com',
+        '0856999666',
+        'customer'
+    ), (
+        'huongnguyen',
+        'huongnguyen123',
+        'active',
+        'Nguyễn Thị Hương',
+        'huongnguyen1234@gmail.com',
+        '0867999555',
+        'customer'
+    ), (
+        'thanhhuong',
+        'thanhhuong123',
+        'active',
+        'Phạm Thanh Hương',
+        'thanhhuong1234@gmail.com',
+        '0908999444',
+        'customer'
+    ), (
+        'minhquan',
+        'minhquan123',
+        'active',
+        'Nguyễn Minh Quân',
+        'minhquan1234@gmail.com',
+        '0969999333',
+        'customer'
+    ), (
+        'ngoctu',
+        'ngoctu123',
+        'active',
+        'Ngọc Tuấn',
+        'ngoctu1234@gmail.com',
+        '0931999222',
+        'customer'
+    ), (
+        'thuonguyen',
+        'thuonguyen123',
+        'active',
+        'Nguyễn Thương Uyên',
+        'thuonguyen1234@gmail.com',
+        '0902999111',
+        'customer'
+    ), (
+        'giangnguyen',
+        'giangnguyen123',
+        'active',
+        'Nguyễn Thị Giang',
+        'giangnguyen1234@gmail.com',
+        '0973999000',
+        'customer'
+    ), (
+        'tuananh',
+        'tuananh123',
+        'active',
+        'Trần Tuấn Anh',
+        'tuananh1234@gmail.com',
+        '0864999888',
+        'customer'
+    )
