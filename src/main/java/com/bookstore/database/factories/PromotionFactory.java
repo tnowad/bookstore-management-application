5df4ec85d07d1092489a7baeb1c9176552ee796b
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-23 10:51:59 +0700

    fix database

--- src/main/java/com/bookstore/database/factories/PromotionFactory.java
+++ src/main/java/com/bookstore/database/factories/PromotionFactory.java
@@ -3,6 +3,7 @@ package com.bookstore.database.factories;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import com.bookstore.model.PromotionModel;
 import com.github.javafaker.Faker;
@@ -14,12 +15,16 @@ public class PromotionFactory implements IFactory<PromotionModel> {
     public PromotionModel create() {
         return new PromotionModel(
                 ++id,
-                faker.lorem().sentence(),
+                faker.commerce().productName(),
                 faker.number().numberBetween(1, 100),
-                faker.date().between(new Date(), Date.from(Instant.now().plus(1, ChronoUnit.DAYS))),
-                faker.date().between(
-                        faker.date().between(new Date(), Date.from(Instant.now().plus(1, ChronoUnit.DAYS))),
-                        Date.from(Instant.now().plus(1, ChronoUnit.WEEKS))),
+                // faker.date().between(new Date(), Date.from(Instant.now().plus(1,
+                // ChronoUnit.DAYS))),
+                // faker.date().between(
+                // faker.date().between(new Date(), Date.from(Instant.now().plus(1,
+                // ChronoUnit.DAYS))),
+                // Date.from(Instant.now().plus(1, ChronoUnit.WEEKS))),
+                faker.date().future(10, TimeUnit.DAYS),
+                faker.date().future(30, TimeUnit.DAYS),
                 faker.lorem().sentence(),
                 faker.number().numberBetween(1, 100),
                 faker.number().numberBetween(5000, 50000));
