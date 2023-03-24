84d222bd2a6cb8853838138e76de14c4529ca8a4
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-25 01:57:43 +0700

    fix

--- src/main/java/com/bookstore/database/seeders/DatabaseSeeder.java
+++ src/main/java/com/bookstore/database/seeders/DatabaseSeeder.java
@@ -4,19 +4,19 @@ public class DatabaseSeeder {
   public static void main(String[] args) {
     ISeeder[] seeders = new ISeeder[] {
         new PromotionSeeder(),
-        // new ImportsSeeder(),
-        // new UserSeeder(),
-        // new BookSeeder(),
-        // new AddressSeeder(),
-        // new AuthorSeeder(),
-        // new CartSeeder(),
-        // new CategorySeeder(),
-        // new PaymentSeeder(),
-        // new PublisherSeeder(),
-        // new ProvidersSeeder(),
-        // new ShippingSeeder(),
-        // new OrderSeeder(),
-        // new EmployeeSeeder(),
+        new ImportsSeeder(),
+        new UserSeeder(),
+        new BookSeeder(),
+        new AddressSeeder(),
+        new AuthorSeeder(),
+        new CartSeeder(),
+        new CategorySeeder(),
+        new PaymentSeeder(),
+        new PublisherSeeder(),
+        new ProvidersSeeder(),
+        new ShippingSeeder(),
+        new OrderSeeder(),
+        new EmployeeSeeder(),
 
     };
     for (ISeeder seeder : seeders) {
