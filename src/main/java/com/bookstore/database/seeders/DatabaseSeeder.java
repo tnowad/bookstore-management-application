2d657a7140749e2b3d00543cd96d49cb7f320125
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2023-03-26 16:09:17 +0700

    ..

--- src/main/java/com/bookstore/database/seeders/DatabaseSeeder.java
+++ src/main/java/com/bookstore/database/seeders/DatabaseSeeder.java
@@ -4,21 +4,6 @@ public class DatabaseSeeder {
   public static void main(String[] args) {
     ISeeder[] seeders = new ISeeder[] {
         new PromotionSeeder(),
-<<<<<<< HEAD
-        // new ImportsSeeder(),
-        // new UserSeeder(),
-        // new BookSeeder(),
-        // new AddressSeeder(),
-        // new AuthorSeeder(),
-        // new CartSeeder(),
-        // new CategorySeeder(),
-        // new PaymentSeeder(),
-        // new PublisherSeeder(),
-        // new ProvidersSeeder(),
-        // new ShippingSeeder(),
-        new OrderSeeder(),
-        // new EmployeeSeeder(),
-=======
         new ImportsSeeder(),
         new UserSeeder(),
         new BookSeeder(),
@@ -36,7 +21,6 @@ public class DatabaseSeeder {
         new ImportDetailSeeder(),
         new CartItemSeeder(),
         new CategoriesSeeder(),
->>>>>>> 563e04fb5c081508ef69d168f535bf84dcec4cc4
 
     };
     for (ISeeder seeder : seeders) {
