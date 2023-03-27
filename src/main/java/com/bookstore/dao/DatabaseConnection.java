8bc6f83fa4fe7827c9957926a40e3047e188fc72
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-27 15:29:00 +0700

    fix(data): fix database connection

--- src/main/java/com/bookstore/dao/DatabaseConnection.java
+++ src/main/java/com/bookstore/dao/DatabaseConnection.java
@@ -18,93 +18,56 @@ public class DatabaseConnection {
   private DatabaseConnection() {
   }
 
-  /**
-   * Returns a connection to the database. This is a lazy operation, so it's safe
-   * to call multiple times without re-creating the connection every time.
-   *
-   * @return a connection to the database or null if there was an error connecting
-   *         to the database or if the connection could not be
-   */
   public static Connection getInstance() {
-    try {
-      if (connection == null) {
+    if (connection == null) {
+      try {
         Class.forName(driver);
         connection = DriverManager.getConnection(url, user, password);
+      } catch (ClassNotFoundException e) {
+        System.err.println("Error: driver not found");
+      } catch (SQLException e) {
+        System.err.println("Error: connection failed");
       }
-    } catch (Exception e) {
-      System.err.println("Error: " + "Connection failed");
     }
     return connection;
   }
 
-  /**
-   * Creates a PreparedStatement with the given SQL. The parameters are set to the
-   * values in the array. If there is an error null is returned
-   *
-   * @param sql - The SQL to be prepared
-   *
-   * @return The prepared statement or null if the connection could not be
-   *         established or the SQL is not valid for the
-   */
-  public static PreparedStatement getPreparedStatement(String sql, Object... args) {
+  public static ResultSet executeQuery(String sql, Object... args) {
     Connection connection = getInstance();
-    if (connection == null)
+    if (connection == null) {
+      System.err.println("Error: " + "Connection failed");
       return null;
-    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    }
+    PreparedStatement preparedStatement = null;
+    try {
+      preparedStatement = connection.prepareStatement(sql);
       for (int i = 0; i < args.length; i++) {
         preparedStatement.setObject(i + 1, args[i]);
       }
-      return preparedStatement;
-    } catch (SQLException e) {
-      System.err.println("Error: " + "PreparedStatement failed");
-    }
-    return null;
-  }
-
-  /**
-   * Executes a query that returns rows. The query is given as a String and can
-   * contain placeholder question marks which will be replaced by the given
-   * arguments
-   *
-   * @param sql - The SQL to be executed.
-   *
-   * @return The result of the query or null if there is an error executing the
-   *         query or the query was not
-   */
-  public static ResultSet executeQuery(String sql, Object... args) {
-    PreparedStatement preparedStatement = getPreparedStatement(sql, args);
-    if (preparedStatement == null)
-      return null;
-    try (ResultSet resultSet = preparedStatement.executeQuery()) {
-      return resultSet;
+      return preparedStatement.executeQuery();
     } catch (SQLException e) {
       System.err.println("Error: " + "Query failed");
     }
     return null;
   }
 
-  /**
-   * Executes an update statement. This method is useful for updating rows in a
-   * table that has been modified or inserted into a table.
-   *
-   * @param sql - The SQL to execute.
-   */
   public static int executeUpdate(String sql, Object... args) {
-    PreparedStatement preparedStatement = getPreparedStatement(sql, args);
-    if (preparedStatement == null)
-      return 0;
-    try {
+    Connection connection = getInstance();
+    if (connection == null) {
+      System.err.println("Error: " + "Connection failed");
+      return -1;
+    }
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      for (int i = 0; i < args.length; i++) {
+        preparedStatement.setObject(i + 1, args[i]);
+      }
       return preparedStatement.executeUpdate();
     } catch (SQLException e) {
       System.err.println("Error: " + "Update failed");
     }
-    return 0;
+    return -1;
   }
 
-  /**
-   * Closes the connection to the database if it exists. This is useful for unit
-   * tests that need to clean up after a test
-   */
   public static void closeConnection() {
     try {
       if (connection != null) {
@@ -115,5 +78,34 @@ public class DatabaseConnection {
     } finally {
       connection = null;
     }
+  }
+
+  public static void main(String[] args) {
+    ResultSet resultSet = DatabaseConnection.executeQuery("show tables");
+    try {
+      while (resultSet.next()) {
+        System.out.println(resultSet.getString(1));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static PreparedStatement getPreparedStatement(String query, Object... args) {
+    Connection connection = getInstance();
+    if (connection == null) {
+      System.err.println("Error: " + "Connection failed");
+      return null;
+    }
+    PreparedStatement preparedStatement = null;
+    try {
+      preparedStatement = connection.prepareStatement(query);
+      for (int i = 0; i < args.length; i++) {
+        preparedStatement.setObject(i + 1, args[i]);
+      }
+    } catch (SQLException e) {
+      System.err.println("Error: " + "Query failed");
+    }
+    return preparedStatement;
   }
 }
