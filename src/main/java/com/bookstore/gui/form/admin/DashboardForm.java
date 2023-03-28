fb7589ba85f333703648bd00007978892273392c
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-28 23:38:55 +0700

    change to jpanel

--- src/main/java/com/bookstore/gui/form/admin/DashboardForm.java
+++ src/main/java/com/bookstore/gui/form/admin/DashboardForm.java
@@ -6,24 +6,14 @@ import javax.swing.JPanel;
 
 import com.bookstore.gui.component.HeaderDashboard;
 
-public class DashboardForm extends JFrame {
+public class DashboardForm extends JPanel {
   public DashboardForm() {
-    JPanel container = new JPanel();
-    container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
     FormHome formHome = new FormHome();
     HeaderDashboard headerDashboard = new HeaderDashboard();
-    container.add(headerDashboard);
-    container.add(formHome);
-    add(container);
-    initComponent();
+    add(headerDashboard);
+    add(formHome);
   }
 
-  private void initComponent() {
-    setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-    setTitle("Dashboard");
-    setResizable(false);
-    setSize(new java.awt.Dimension(800, 600));
-    setLocationRelativeTo(null);
-  }
 }
