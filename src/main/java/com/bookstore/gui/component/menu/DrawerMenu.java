eb11e23e7240ac853c5b14ffe479c4d6fadec8ef
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-18 01:55:40 +0700

    add bound

--- src/main/java/com/bookstore/gui/component/menu/DrawerMenu.java
+++ src/main/java/com/bookstore/gui/component/menu/DrawerMenu.java
@@ -1,18 +1,6 @@
 package com.bookstore.gui.component.menu;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
-
-import javax.swing.DefaultListModel;
-import javax.swing.Icon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ListCellRenderer;
 
 import com.bookstore.models.MenuItemModel;
 import com.bookstore.models.MenuModel;
@@ -22,62 +10,17 @@ public class DrawerMenu extends JPanel {
 
     public DrawerMenu(MenuModel menuModel) {
         this.menuModel = menuModel;
+    }
 
-        // Create a JList to display the menu items
-        JList<MenuItemModel> list = new JList<>();
-        list.setModel(new DefaultListModel<>());
-        list.setCellRenderer(new MenuItemRenderer());
+    public MenuModel getMenuModel() {
+        return menuModel;
+    }
 
-        // Add the menu items to the list
-        DefaultListModel<MenuItemModel> listModel = (DefaultListModel<MenuItemModel>) list.getModel();
-        List<MenuItemModel> menuItems = menuModel.getMenuItems();
-        for (MenuItemModel menuItem : menuItems) {
-            listModel.addElement(menuItem);
-        }
+    public void setMenuModel(MenuModel menuModel) {
+        this.menuModel = menuModel;
+    }
 
-        // Add an ActionListener to the list to handle selection
+    public void addMenuItem(MenuItemModel menuItemModel) {
         
-        for (MenuItemModel menuItemModel : menuItems) {
-            listModel.addElement(menuItemModel);
-        }
-
-        // Add the list to the JPanel
-        setLayout(new BorderLayout());
-        add(new JLabel(menuModel.getTitle(), menuModel.getIcon(), JLabel.CENTER), BorderLayout.NORTH);
-        add(new JScrollPane(list), BorderLayout.CENTER);
-    }
-
-    // Custom ListCellRenderer to display the menu item titles
-    private class MenuItemRenderer extends JLabel implements ListCellRenderer<MenuItemModel> {
-        public MenuItemRenderer() {
-            setOpaque(true);
-        }
-
-        @Override
-        public java.awt.Component getListCellRendererComponent(JList<? extends MenuItemModel> list, MenuItemModel value,
-                int index, boolean isSelected, boolean cellHasFocus) {
-            setText(value.getTitle());
-            if (isSelected) {
-                setBackground(list.getSelectionBackground());
-                setForeground(list.getSelectionForeground());
-            } else {
-                setBackground(list.getBackground());
-                setForeground(list.getForeground());
-            }
-            return this;
-        }
-    }
-
-    public static void main(String[] args) {
-        MenuModel menuModel = new MenuModel(null, "Menu Title", null, List.of(
-                new MenuItemModel("Item 1", "action1"),
-                new MenuItemModel("Item 2", "action2"),
-                new MenuItemModel("Item 3", "action3")));
-        JPanel drawerMenu = new DrawerMenu(menuModel);
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(drawerMenu);
-        frame.pack();
-        frame.setVisible(true);
     }
 }
