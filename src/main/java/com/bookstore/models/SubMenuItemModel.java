86e4ca4315530c174809a444d4b76d3ffd06c314
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-18 21:08:29 +0700

    add setter getter

--- src/main/java/com/bookstore/models/SubMenuItemModel.java
+++ src/main/java/com/bookstore/models/SubMenuItemModel.java
@@ -14,4 +14,29 @@ public class SubMenuItemModel {
         this.icon = icon;
         this.actionListener = actionListener;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
+
+    public ActionListener getActionListener() {
+        return actionListener;
+    }
+
+    public void setActionListener(ActionListener actionListener) {
+        this.actionListener = actionListener;
+    }
+
 }
