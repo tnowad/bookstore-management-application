c01ff3ade328069e2e4807da667726872dbe2b2a
HOANGGIABAO212 <hoanggiabao2122003@gmail.com>
2023-04-20 00:05:39 +0700

    code current user MODEL,DAO, BUS

---
+++ src/main/java/com/bookstore/bus/CurrentUserBUS.java
@@ -0,0 +1,84 @@
+package com.bookstore.bus;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.bookstore.dao.CurrentUserDAO;
+import com.bookstore.interfaces.IBUS;
+import com.bookstore.models.CurrentUserModel;
+public class CurrentUserBUS implements IBUS<CurrentUserModel> {
+    private final List<CurrentUserModel> currentUserList = new ArrayList<>();
+    private static CurrentUserBUS instance;
+
+    public static CurrentUserBUS getInstance() {
+        if (instance == null) {
+            instance = new CurrentUserBUS();
+        }
+        return instance;
+    }
+
+    private CurrentUserBUS() {
+        this.currentUserList.addAll(CurrentUserDAO.getInstance().readDatabase());
+    }
+
+    @Override
+    public List<CurrentUserModel> getAllModels() {
+        return Collections.unmodifiableList(currentUserList);
+    }
+
+    @Override
+    public CurrentUserModel getModelById(int id) {
+        for (CurrentUserModel currentUserModel : currentUserList) {
+            if (currentUserModel.getCurrentUserId() == id) {
+                return currentUserModel;
+            }
+        }
+        return null;
+    }
+
+    private CurrentUserModel mapToEntity(CurrentUserModel from) {
+        return new CurrentUserModel(from.getCurrentUserId());
+    }
+
+    @Override
+    public int addModel(CurrentUserModel currentUserModel) {
+        int id = CurrentUserDAO.getInstance().insert(mapToEntity(currentUserModel));
+        currentUserModel.setCurrentUserId(id);
+        currentUserList.add(currentUserModel);
+        return id;
+    }
+
+    @Override
+    public int updateModel(CurrentUserModel currentUserModel) {
+        int updatedRows = CurrentUserDAO.getInstance().update(mapToEntity(currentUserModel));
+        if (updatedRows > 0) {
+            for (int i = 0; i < currentUserList.size(); i++) {
+                if (currentUserList.get(i).getCurrentUserId() == currentUserModel.getCurrentUserId()) {
+                    currentUserList.set(i, currentUserModel);
+                    break;
+                }
+            }
+        }
+        return updatedRows;
+    }
+
+    @Override
+    public int deleteModel(int id) {
+        CurrentUserModel currentUserModel = getModelById(id);
+        if (currentUserModel == null) {
+            throw new IllegalArgumentException("Current user with ID " + id + " does not exist.");
+        }
+        int deletedRows = CurrentUserDAO.getInstance().delete(id);
+        if (deletedRows > 0) {
+            currentUserList.remove(currentUserModel);
+        }
+        return deletedRows;
+    }
+
+    @Override
+    public List<CurrentUserModel> searchModel(String value, String[] columns) {
+        throw new UnsupportedOperationException("Search method is not supported in CurrentUserBUS");
+    }
+}
---
+++ src/main/java/com/bookstore/dao/CurrentUserDAO.java
@@ -0,0 +1,79 @@
+package com.bookstore.dao;
+import com.bookstore.models.CurrentUserModel;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.bookstore.interfaces.IDAO;
+public class CurrentUserDAO implements IDAO<CurrentUserModel> {
+    private static CurrentUserDAO instance;
+
+    public static CurrentUserDAO getInstance() {
+        if (instance == null) {
+            instance = new CurrentUserDAO();
+        }
+        return instance;
+    }
+
+    private CurrentUserModel createCurrentUserModelFromResultSet(ResultSet rs) throws SQLException {
+        return new CurrentUserModel(rs.getInt("current_user_id"));
+    }
+
+    @Override
+    public ArrayList<CurrentUserModel> readDatabase() {
+        ArrayList<CurrentUserModel> currentUserList = new ArrayList<>();
+        try (ResultSet rs = DatabaseConnection.executeQuery("SELECT * FROM current_user")) {
+            while (rs.next()) {
+                CurrentUserModel currentUserModel = createCurrentUserModelFromResultSet(rs);
+                currentUserList.add(currentUserModel);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return currentUserList;
+    }
+
+    @Override
+    public int insert(CurrentUserModel currentUser) {
+        String insertSql = "INSERT INTO current_user (current_user_id) VALUES (?)";
+        Object[] args = {currentUser.getCurrentUserId()};
+        try {
+            return DatabaseConnection.executeUpdate(insertSql, args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public int update(CurrentUserModel currentUser) {
+        String updateSql = "UPDATE current_user SET current_user_id = ?";
+        Object[] args = {currentUser.getCurrentUserId()};
+        try {
+            return DatabaseConnection.executeUpdate(updateSql, args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public int delete(int id) {
+        String deleteSql = "DELETE FROM current_user WHERE current_user_id = ?";
+        Object[] args = {id};
+        try {
+            return DatabaseConnection.executeUpdate(deleteSql, args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public List<CurrentUserModel> search(String condition, String[] columnNames) {
+        throw new UnsupportedOperationException("Search method is not supported in CurrentUserDAO");
+    }
+}
