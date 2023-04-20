eed8f2eca5d215e2ee083dd62c8f4b2975915b80
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-20 10:01:45 +0700

    remove conflict

--- src/main/java/com/bookstore/gui/component/menu/MenuButton.java
+++ src/main/java/com/bookstore/gui/component/menu/MenuButton.java
@@ -10,22 +10,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.Icon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.border.EmptyBorder;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
 
 public class MenuButton extends JButton {
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    private int index;
     private Animator animator;
     private int targetSize;
     private float animatSize;
@@ -68,6 +59,7 @@ public class MenuButton extends JButton {
                 animator.start();
             }
         });
+
         TimingTarget target = new TimingTargetAdapter() {
             @Override
             public void timingEvent(float fraction) {
@@ -83,16 +75,28 @@ public class MenuButton extends JButton {
     }
 
     @Override
-    protected void paintComponent(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    protected void paintComponent(Graphics graphics) {
+        Graphics2D graphics2d = (Graphics2D) graphics;
+        graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         if (pressedPoint != null) {
-            g2.setColor(effectColor);
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-            g2.fillOval((int) (pressedPoint.x - animatSize / 2), (int) (pressedPoint.y - animatSize / 2),
+            graphics2d.setColor(effectColor);
+            graphics2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+            graphics2d.fillOval((int) (pressedPoint.x - animatSize / 2), (int) (pressedPoint.y - animatSize / 2),
                     (int) animatSize, (int) animatSize);
         }
-        g2.setComposite(AlphaComposite.SrcOver);
-        super.paintComponent(grphcs);
+        graphics2d.setComposite(AlphaComposite.SrcOver);
+        super.paintComponent(graphics);
+    }
+
+    public static void main(String[] args) {
+        MenuButton menuButton = new MenuButton(null, "Menu");
+        menuButton.setSize(100, 100);
+        menuButton.setVisible(true);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(menuButton);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
