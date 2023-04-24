2debe684bf49201a1f97bdf8a05d11ffed3f5d23
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2023-04-24 11:54:36 +0700

    Fixed some bugs.

--- src/main/java/com/bookstore/gui/form/salesman/view/PendingOrderList.java
+++ src/main/java/com/bookstore/gui/form/salesman/view/PendingOrderList.java
@@ -3,14 +3,6 @@ package com.bookstore.gui.form.salesman.view;
 import javax.swing.*;
 import javax.swing.JTextField;
 
-import com.bookstore.bus.OrderBUS;
-import com.bookstore.gui.component.button.Button;
-import com.bookstore.gui.component.button.Label;
-
-import java.util.List;
-import javax.swing.table.DefaultTableModel;
-import com.bookstore.models.OrderModel;
-
 public class PendingOrderList extends JPanel {
 
     public PendingOrderList() {
