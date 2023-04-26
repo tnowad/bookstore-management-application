e3976e89b8224fb4aa86c60eab8aac2a8bc239c5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-26 23:50:01 +0700

    add Action Panel

--- src/main/java/com/bookstore/gui/component/panel/ActionPanel.java
+++ src/main/java/com/bookstore/gui/component/panel/ActionPanel.java
@@ -4,6 +4,7 @@ import com.bookstore.gui.component.button.ActionButton;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -21,6 +22,7 @@ public class ActionPanel extends JPanel {
 
   private void initComponents() {
     layout = new FlowLayout(FlowLayout.CENTER, 10, 10);
+    setLayout(layout);
 
     buttonView = new ActionButton();
     buttonUpdate = new ActionButton();
@@ -39,11 +41,39 @@ public class ActionPanel extends JPanel {
     add(buttonDelete);
   }
 
+  public void addViewListener(ActionListener listener) {
+    buttonView.addActionListener(listener);
+  }
+
+  public void addUpdateListener(ActionListener listener) {
+    buttonUpdate.addActionListener(listener);
+  }
+
+  public void addDeleteListener(ActionListener listener) {
+    buttonDelete.addActionListener(listener);
+  }
+
   public static void main(String[] args) {
     FlatMacLightLaf.setup();
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(new ActionPanel());
+    frame
+      .getContentPane()
+      .add(
+        new ActionPanel() {
+          {
+            addViewListener(e -> {
+              System.out.println("View");
+            });
+            addUpdateListener(e -> {
+              System.out.println("Update");
+            });
+            addDeleteListener(e -> {
+              System.out.println("Delete");
+            });
+          }
+        }
+      );
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
