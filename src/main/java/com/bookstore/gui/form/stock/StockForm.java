29760862c9fd0569e12bb938532d230eb255bcae
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-26 19:26:39 +0700

    add test table

--- src/main/java/com/bookstore/gui/form/stock/StockForm.java
+++ src/main/java/com/bookstore/gui/form/stock/StockForm.java
@@ -1,25 +1,62 @@
 package com.bookstore.gui.form.stock;
 
+import com.bookstore.gui.component.table.Table;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.LayoutManager;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class StockForm extends JPanel {
 
   private static final long serialVersionUID = 1L;
   private LayoutManager layout;
+  private Table table;
 
   public StockForm() {
     initComponents();
+    table.setModel(
+      new DefaultTableModel(
+        new Object[][] {},
+        new String[] { "Name", "Email", "User Type", "Joined", "Status" }
+      ) {
+        private static final long serialVersionUID = 1L;
+        Class<?>[] types = new Class[] {
+          java.lang.String.class,
+          java.lang.String.class,
+          java.lang.String.class,
+          java.lang.String.class,
+          java.lang.Boolean.class,
+        };
+        boolean[] canEdit = new boolean[] { false, false, false, false, false };
+
+        public Class<?> getColumnClass(int columnIndex) {
+          return types[columnIndex];
+        }
+
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+          return canEdit[columnIndex];
+        }
+      }
+    );
+    table.addRow(
+      new Object[] {
+        "Mike Bhand",
+        "mikebhand@gmail.com",
+        "Admin",
+        "25 Apr,2018",
+        true,
+      }
+    );
   }
 
   private void initComponents() {
     layout = new BorderLayout();
     setLayout(layout);
-    add(new StockTable(), BorderLayout.CENTER);
+    table = new Table();
+    add(table, BorderLayout.CENTER);
   }
 
   public static void main(String[] args) {
