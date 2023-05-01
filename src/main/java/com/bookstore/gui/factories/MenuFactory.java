9297d83ff7fe92a9504d0b6dfbd5a0ca209618f6
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-01 17:04:29 +0700

    refactor

--- src/main/java/com/bookstore/gui/factories/MenuFactory.java
+++ src/main/java/com/bookstore/gui/factories/MenuFactory.java
@@ -1,19 +1,31 @@
 package com.bookstore.gui.factories;
 
 import com.bookstore.dao.EmployeeDAO;
-import com.bookstore.gui.form.salesman.view.SalesmanFrame;
 import com.bookstore.models.EmployeeModel;
 import com.bookstore.models.MenuItemModel;
 import com.bookstore.models.MenuModel;
 import com.bookstore.models.SubMenuItemModel;
 import com.bookstore.models.UserModel;
-import com.bookstore.models.UserModel.Role;
-import java.awt.MenuItem;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 public class MenuFactory {
+
+  public static MenuModel getMenu(UserModel user) {
+    switch (user.getRole()) {
+      case ADMIN:
+        return getAdminMenu();
+      case CUSTOMER:
+        return getCustomerMenu();
+      case EMPLOYEE:
+        return getMenuEmployee(
+          EmployeeDAO.getInstance().getEmployeeById(user.getId())
+        );
+      default:
+        return new MenuModel(new ArrayList<MenuItemModel>());
+    }
+  }
 
   public static MenuModel getAdminMenu() {
     return new MenuModel(
@@ -59,21 +71,6 @@ public class MenuFactory {
 
   public static MenuModel getCustomerMenu() {
     return new MenuModel(null);
-  }
-
-  public static MenuModel getMenu(UserModel user) {
-    switch (user.getRole()) {
-      case ADMIN:
-        return getAdminMenu();
-      case CUSTOMER:
-        return getCustomerMenu();
-      case EMPLOYEE:
-        return getMenuEmployee(
-          EmployeeDAO.getInstance().getEmployeeById(user.getId())
-        );
-      default:
-        return new MenuModel(new ArrayList<MenuItemModel>());
-    }
   }
 
   public static MenuModel getMenuEmployee(EmployeeModel employee) {
