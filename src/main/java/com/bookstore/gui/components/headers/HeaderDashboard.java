ff2c963ea21e3ce5ed3ebddb701a1bfb93a2374b
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-03 03:14:58 +0700

    fix header

--- src/main/java/com/bookstore/gui/components/headers/HeaderDashboard.java
+++ src/main/java/com/bookstore/gui/components/headers/HeaderDashboard.java
@@ -2,18 +2,13 @@ package com.bookstore.gui.components.headers;
 
 import com.bookstore.gui.components.inputs.SearchText;
 import com.bookstore.gui.main.MainUI;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.LayoutStyle;
 
 public class HeaderDashboard extends JPanel {
 
@@ -49,88 +44,25 @@ public class HeaderDashboard extends JPanel {
     buttonMenu.setIcon(new ImageIcon("src/main/java/resources/icons/menu.png"));
     buttonMenu.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     buttonMenu.addActionListener(e -> {
-      // get parent frame of this panel
       MainUI mainUI = (MainUI) getRootPane().getParent();
-      // toggle menu
       mainUI.toggleMenu();
     });
-    // Create layout for header with labelMenu, searchText, and labelSearch
-    // search text is in the center of the header
-    GroupLayout layout = new GroupLayout(this);
-    setLayout(layout);
-    layout.setHorizontalGroup(
-      layout
-        .createParallelGroup(GroupLayout.Alignment.LEADING)
-        .addGroup(
-          layout
-            .createSequentialGroup()
-            .addComponent(buttonMenu)
-            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(
-              searchText,
-              GroupLayout.DEFAULT_SIZE,
-              300,
-              Short.MAX_VALUE
-            )
-            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(labelSearch)
-        )
-    );
-    layout.setVerticalGroup(
-      layout
-        .createParallelGroup(GroupLayout.Alignment.LEADING)
-        .addGroup(
-          layout
-            .createSequentialGroup()
-            .addGroup(
-              layout
-                .createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addComponent(buttonMenu)
-                .addComponent(
-                  searchText,
-                  GroupLayout.PREFERRED_SIZE,
-                  GroupLayout.DEFAULT_SIZE,
-                  GroupLayout.PREFERRED_SIZE
-                )
-                .addComponent(labelSearch)
-            )
-        )
-    );
-    searchText.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    buttonMenu.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    labelSearch.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    buttonMenu.setOpaque(false);
-    labelSearch.setOpaque(false);
-    searchText.setOpaque(false);
-    buttonMenu.setBackground(new java.awt.Color(255, 255, 255));
-    labelSearch.setBackground(new java.awt.Color(255, 255, 255));
-    searchText.setBackground(new java.awt.Color(255, 255, 255));
-  }
-
-  @Override
-  protected void paintComponent(Graphics graphics) {
-    Graphics2D graphics2d = (Graphics2D) graphics;
-    graphics2d.setRenderingHint(
-      RenderingHints.KEY_ANTIALIASING,
-      RenderingHints.VALUE_ANTIALIAS_ON
-    );
-    graphics2d.setColor(getBackground());
-    graphics2d.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
-    graphics2d.fillRect(0, 0, 25, getHeight());
-    graphics2d.fillRect(
-      getWidth() - 25,
-      getHeight() - 25,
-      getWidth(),
-      getHeight()
-    );
-    super.paintComponent(graphics);
-  }
-
-  public static void main(String[] args) {
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(new HeaderDashboard());
-    frame.pack();
-    frame.setVisible(true);
+    setLayout(new GridBagLayout());
+    GridBagConstraints gridBagConstraints = new GridBagConstraints();
+    gridBagConstraints.fill = GridBagConstraints.NONE;
+    gridBagConstraints.anchor = GridBagConstraints.WEST;
+    add(buttonMenu, gridBagConstraints);
+    gridBagConstraints = new GridBagConstraints();
+    gridBagConstraints.gridx = 1;
+    gridBagConstraints.weightx = 1;
+    gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+    gridBagConstraints.anchor = GridBagConstraints.CENTER;
+    add(searchText, gridBagConstraints);
+    gridBagConstraints = new GridBagConstraints();
+    gridBagConstraints.gridx = 2;
+    gridBagConstraints.weightx = 0;
+    gridBagConstraints.fill = GridBagConstraints.NONE;
+    gridBagConstraints.anchor = GridBagConstraints.EAST;
+    add(labelSearch, gridBagConstraints);
   }
 }
