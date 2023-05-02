21ce9b1f964c742c6ae7a6897e432dfd6c0af005
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-02 12:11:10 +0700

    remove outline

--- src/main/java/com/bookstore/gui/factories/MenuFactory.java
+++ src/main/java/com/bookstore/gui/factories/MenuFactory.java
@@ -1,5 +1,6 @@
 package com.bookstore.gui.factories;
 
+import com.bookstore.dao.EmployeeDAO;
 import com.bookstore.gui.components.dashboards.DashboardPanel;
 import com.bookstore.gui.components.panels.MainPanel;
 import com.bookstore.gui.forms.users.SalesmanFrame;
@@ -22,9 +23,9 @@ public class MenuFactory {
       case CUSTOMER:
         return getCustomerMenu();
       case EMPLOYEE:
-        // return getMenuEmployee(
-        //     EmployeeDAO.getInstance().getEmployeeById(user.getId()));
-        new SalesmanFrame();
+        return getMenuEmployee(
+          EmployeeDAO.getInstance().getEmployeeById(user.getId())
+        );
       default:
         throw new IllegalArgumentException("User role is not supported");
     }
@@ -43,67 +44,81 @@ public class MenuFactory {
 
   public static MenuModel getAdminMenu() {
     return new MenuModel(
-        new ArrayList<MenuItemModel>() {
-          {
-            add(
-                new MenuItemModel(
-                    "Dashboard",
-                    new ImageIcon(""),
-                    showHomeAdmin,
-                    null));
-            add(
-                new MenuItemModel(
-                    "Customer",
-                    new ImageIcon(""),
-                    showHomeAdmin,
-                    new ArrayList<SubMenuItemModel>() {
-                      {
-                        add(
-                            new SubMenuItemModel(
-                                "Add Customer",
-                                new ImageIcon(""),
-                                showHomeAdmin));
-                        add(
-                            new SubMenuItemModel(
-                                "View Customer",
-                                new ImageIcon(""),
-                                showHomeAdmin));
-                        add(
-                            new SubMenuItemModel(
-                                "Edit Customer",
-                                new ImageIcon(""),
-                                showHomeAdmin));
-                      }
-                    }));
-          }
-        });
+      new ArrayList<MenuItemModel>() {
+        {
+          add(
+            new MenuItemModel(
+              "Dashboard",
+              new ImageIcon(""),
+              showHomeAdmin,
+              null
+            )
+          );
+          add(
+            new MenuItemModel(
+              "Customer",
+              new ImageIcon(""),
+              showHomeAdmin,
+              new ArrayList<SubMenuItemModel>() {
+                {
+                  add(
+                    new SubMenuItemModel(
+                      "Add Customer",
+                      new ImageIcon(""),
+                      showHomeAdmin
+                    )
+                  );
+                  add(
+                    new SubMenuItemModel(
+                      "View Customer",
+                      new ImageIcon(""),
+                      showHomeAdmin
+                    )
+                  );
+                  add(
+                    new SubMenuItemModel(
+                      "Edit Customer",
+                      new ImageIcon(""),
+                      showHomeAdmin
+                    )
+                  );
+                }
+              }
+            )
+          );
+        }
+      }
+    );
   }
 
   public static MenuModel getCustomerMenu() {
     return new MenuModel(
-        new ArrayList<MenuItemModel>() {
-          {
-            add(new MenuItemModel("Home", new ImageIcon(""), null, null));
-          }
-        });
+      new ArrayList<MenuItemModel>() {
+        {
+          add(new MenuItemModel("Home", new ImageIcon(""), null, null));
+        }
+      }
+    );
   }
 
   public static MenuModel getMenuEmployeeSalesman() {
     return new MenuModel(
-        new ArrayList<MenuItemModel>() {
-          {
-            add(new MenuItemModel("Home", new ImageIcon(""), null, null));
-          }
-        });
+      new ArrayList<MenuItemModel>() {
+        {
+          add(new MenuItemModel("Home", new ImageIcon(""), null, null));
+        }
+      }
+    );
   }
 
   public static MenuModel getMenuEmployeeManager() {
     return new MenuModel(
-        new ArrayList<MenuItemModel>() {
-          {
-            add(new MenuItemModel("Home", new ImageIcon(""), null, null));
-          }
-        });
+      new ArrayList<MenuItemModel>() {
+        {
+          add(new MenuItemModel("Home", new ImageIcon(""), null, null));
+        }
+      }
+    );
   }
 
   private static ActionListener showHomeAdmin = e -> {
