b4b449603fdd5a2af698ee5fa8ed01726bf58ce0
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-05 18:06:31 +0700

    add panel filter

---
+++ src/main/java/com/bookstore/gui/forms/filters/BookFilterForm.java
@@ -0,0 +1,17 @@
+package com.bookstore.gui.forms.filters;
+
+import javax.swing.JPanel;
+
+public class BookFilterForm extends JPanel {
+
+  // show list checkbox (show all, show available, show unavailable) (default: show all)
+  // show list by (title, author, genre, publisher, isbn, year, price) (default: title)
+  // show list by (ascending, descending) (default: ascending)
+  // show list by (price, year) (default: price)
+
+  private BookFilterForm() {
+    initComponents();
+  }
+
+  private void initComponents() {}
+}
